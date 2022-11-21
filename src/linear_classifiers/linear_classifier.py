b5150463aecb2162bd609985b303a5e32eeafdef
Brendan Schlaman <brendan.schlaman@gmail.com>
2022-11-20 19:25:27 -0800

    build out classifier more

--- src/linear_classifiers/linear_classifier.py
+++ src/linear_classifiers/linear_classifier.py
@@ -1,31 +1,63 @@
 from abc import ABC, abstractmethod
+import collections
+import random
+
 from utils.math.vectors import Vector
 from linear_classifiers.data_generator import data_iter, get_data_stats
+
+
+def transform_raw_data(raw_data: list[dict[str, str]]) -> tuple[list[str], bool]:
+    """Convert raw data into the format the classifiers expect"""
+    label_key = "real"
+    features_key = "title"
+    data = []
+    for rd in raw_data:
+        data.append((rd[features_key].split(), bool(rd[label_key])))
+    return data
 
 
 class LinearClassifier(ABC):
     w: Vector
 
-    def __init__(self, data: list[Vector], labels: list):
-        assert len(data) == len(labels)
-        self.data = data
-        self.labels = labels
+    def __init__(self, data: tuple[list[str], bool]):
+        self.data = []
+        self.m = 6  # number of words per datum
+        for features, label in data:
+            self.data.append((self._sample_features(features), label))
 
-    def _build_features(words: list[str]) -> collections.Counter:
-        """Convert raw data into feature vectors than can be operated on
-        This must be the same process for all classifiers
-        """
+    def _sample_features(self, features: list[str]) -> collections.Counter:
+        """Convert raw data into feature vectors that can be operated on"""
         # not sure if this is the same as the dimensionality
         # since there will likely be >>6 distinct words across data points
-        num_words = 6
-        return collections.Counter(random.choices(words, k=num_words))
+        return collections.Counter(random.choices(features, k=self.m))
+
+    # core components of a classifier
+    @abstractmethod
+    def calculate_parameter(self, c: int, alpha: str) -> float:
+        """Estimate θ^_αc for a generative algorithm with
+        multinomial features
+        """
+        pass
 
 
 class NaiveBayesClassifier(LinearClassifier):
     def __init__(self):
         get_data_stats()
-        super().__init__([Vector(1, 2, 3)], [123])
-        # generative learning
+        raw_data = list(data_iter())[:10]
+        super().__init__(transform_raw_data(raw_data))
 
-    def _build_features():
-        pass
+    def calculate_parameter(self, c: int, alpha: str) -> float:
+        """Estimate θ^_αc for a generative algorithm with
+        multinomial features
+        """
+        # TODO (2022.11.20) add smoothing; should I assume I've seen each word once?
+        # sum(I(y_i == c)x_iα); i.e. number of times word α
+        # has appeared across all data points
+        alpha_count = 0
+        # TODO (2022.11.20) is this the correct denominator?
+        word_count = self.m * len(self.data)
+        for features, label in self.data:
+            if label != c:
+                continue
+            alpha_count += features[alpha]
+        return alpha_count / word_count
